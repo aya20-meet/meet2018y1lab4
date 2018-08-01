2cb2e484782920bd6b7e4b77898e69958fb17b9c
aya20-meet <aya20@meet.mit.edu>
2018-08-01 03:36:13 -0400

    Stopping here

---
+++ FruitGame.py
@@ -0,0 +1,18 @@
+Python 3.6.5 (default, Apr  1 2018, 05:46:30) 
+[GCC 7.3.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+====== RESTART: /home/student/aya20_lab4/meet2018y1lab4/Fruit_Store.py ======
+what fruit an i sorthing? appels
+Traceback (most recent call last):
+  File "/home/student/aya20_lab4/meet2018y1lab4/Fruit_Store.py", line 2, in <module>
+    appels = bin1
+NameError: name 'bin1' is not defined
+>>> 
+====== RESTART: /home/student/aya20_lab4/meet2018y1lab4/Fruit_Store.py ======
+what fruit an i sorthing? appels
+Traceback (most recent call last):
+  File "/home/student/aya20_lab4/meet2018y1lab4/Fruit_Store.py", line 2, in <module>
+    if appels:
+NameError: name 'appels' is not defined
+>>> 
---
+++ Fruit_Store.py
@@ -0,0 +1,7 @@
+fruit = input('what fruit an i sorthing?')
+if appels:
+    print('bin1')
+elif oranges:
+    print(bin2)
+else:
+    print('error i dont recognise this fruit')
